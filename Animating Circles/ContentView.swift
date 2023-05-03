ba21ae5a5d1b07e3607a7e42384de1177ba55e95
Asadbek <asadbekn443@gmail.com>
2023-05-03 01:38:13 -0400

    Added animation

--- Animating Circles/ContentView.swift
+++ Animating Circles/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var scaleInOut = false
+    @State var rotateInOut = false
+    @State var moveInOut = false
+    @State private var rotationAmount: Double = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Rectangle()
+                .ignoresSafeArea()
+            ZStack{
+                
+                //MARK: - Circles Set 1
+                ZStack{
+                    Circle().fill(.linearGradient(colors: [.green, .white], startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    Circle().fill(.linearGradient(colors: [.green, .white], startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                
+                //MARK: - Circles Set 2
+                ZStack{
+                    Circle().fill(.linearGradient(colors: [.green, .white], startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    Circle().fill(.linearGradient(colors: [.green, .white], startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5).rotationEffect(.degrees(60))
+                
+                //MARK: - Circles Set 3
+                ZStack{
+                    Circle().fill(.linearGradient(colors: [.green, .white], startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    Circle().fill(.linearGradient(colors: [.green, .white], startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5).rotationEffect(.degrees(120))
+                
+            }
+            
+            .rotationEffect(.degrees(rotateInOut ? 90 : 0))
+            .scaleEffect(scaleInOut ? 1 : 0.5)
+            .animation(Animation
+                .easeInOut(duration: 0.1)
+                .repeatForever(autoreverses: true)
+                .speed(1/8),
+                       value: rotationAmount)
+            .onAppear {
+                rotationAmount = 3
+                moveInOut.toggle()
+                scaleInOut.toggle()
+                rotateInOut.toggle()
+            }
         }
-        .padding()
     }
 }
 
